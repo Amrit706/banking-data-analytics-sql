e4118399a7485aa7d8c093128d559357eba5662c
Amrit Kumar <amritkumar83728@gmail.com>
2026-08-24 15:06:20 +0530

    Customer analysis: distribution by age, customer type, and location

--- sql/02_customer_analysis.sql
+++ sql/02_customer_analysis.sql
@@ -7,3 +7,34 @@ USE finance_fraud_loans;
 
 SELECT COUNT(DISTINCT customerID) AS unique_customer_count
 FROM customers_cleaned;
+
+-- • How are customers distributed by age (derived from DateOfBirth), customer type, and location (using the Addresses table)?
+
+-- Distribution of customers by age
+
+SELECT CASE WHEN DateOfBirth_clean IS NULL THEN 'Unknown'
+		ELSE CAST(TIMESTAMPDIFF(YEAR, DateOfBirth_clean, CURDATE()) AS CHAR) 
+        END AS age,
+COUNT(customerID) AS customer_count
+FROM customers_cleaned
+GROUP BY age
+ORDER BY age ASC;
+
+-- NOTE - here age = 0 , showing that the age is less than a year.
+
+-- Distribution of customers by customer type
+
+SELECT t2.TypeName AS customer_type, COUNT(t1.customerID) AS customer_count
+FROM customers_cleaned AS t1
+JOIN customer_types AS t2
+ON t1.CustomerTypeID = t2.CustomerTypeID
+GROUP BY customer_type
+ORDER BY customer_count;
+
+-- Distribution of customers by locations
+
+SELECT t2.Country, t2.City, COUNT(t1.customerID) AS customer_count
+FROM customers_cleaned AS t1
+JOIN addresses AS t2
+ON t1.AddressID = t2.AddressID
+GROUP BY t2.Country, t2.City;
