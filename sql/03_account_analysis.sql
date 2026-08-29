1cfffea7e8cdf3436d2ee6ab97be5fb214c20907
Amrit Kumar <amritkumar83728@gmail.com>
2026-08-29 19:41:18 +0530

    Number of active accounts by types

--- sql/03_account_analysis.sql
+++ sql/03_account_analysis.sql
@@ -6,9 +6,23 @@ USE finance_fraud_loans;
 
 -- • Number of accounts by account type
 
+-- overall 
+
 SELECT t2.TypeName AS type, COUNT(t1.AccountID) AS num_of_acc
 FROM accounts AS t1
 INNER JOIN account_types AS t2
 ON t1.AccountTypeID = t2.AccountTypeID
+GROUP BY type
+ORDER BY num_of_acc DESC;
+
+-- active accounts
+
+SELECT t3.TypeName AS type, COUNT(t1.AccountID) AS num_of_acc
+FROM accounts AS t1
+INNER JOIN account_statuses AS t2
+ON t1.AccountStatusID = t2.AccountStatusID
+INNER JOIN account_types AS t3
+ON t1.AccountTypeID = t3.AccountTypeID
+WHERE t2.StatusName = 'Active'
 GROUP BY type
 ORDER BY num_of_acc DESC;
