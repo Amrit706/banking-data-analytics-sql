a3002eb24fe9cfd3c0c797f715f6bf0b1e00511a
Amrit Kumar <amritkumar83728@gmail.com>
2026-08-29 19:44:04 +0530

    Total balance of active acccounts by its types

--- sql/03_account_analysis.sql
+++ sql/03_account_analysis.sql
@@ -26,3 +26,16 @@ ON t1.AccountTypeID = t3.AccountTypeID
 WHERE t2.StatusName = 'Active'
 GROUP BY type
 ORDER BY num_of_acc DESC;
+
+-- • Total balance by account type (Active accounts)
+
+SELECT t3.TypeName AS type, ROUND(SUM(t1.Balance),2) AS total_balance
+FROM accounts AS t1
+INNER JOIN account_statuses AS t2
+ON t1.AccountStatusID = t2.AccountStatusID
+INNER JOIN account_types AS t3
+ON t1.AccountTypeID = t3.AccountTypeID
+WHERE t2.StatusName = 'Active'
+GROUP BY type
+ORDER BY total_balance DESC;
+
