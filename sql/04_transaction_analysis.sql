e9756a5bccdb46f4a35ac0b072ce1bc75fe77a31
Amrit Kumar <amritkumar83728@gmail.com>
2026-09-01 20:09:44 +0530

    Total transaction value

--- sql/04_transaction_analysis.sql
+++ sql/04_transaction_analysis.sql
@@ -10,3 +10,8 @@ USE finance_fraud_loans;
 
 SELECT COUNT(*) AS total_transactions_counts
 FROM transactions;
+
+-- • What is the total transaction value?
+
+SELECT ROUND(SUM(Amount),2) AS total_transaction_amount
+FROM transactions;
