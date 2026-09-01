3262f117501b9f286f581bed228b8fdac9ca247e
Amrit Kumar <amritkumar83728@gmail.com>
2026-09-01 20:19:03 +0530

    Most common transaction types

--- sql/04_transaction_analysis.sql
+++ sql/04_transaction_analysis.sql
@@ -15,3 +15,12 @@ FROM transactions;
 
 SELECT ROUND(SUM(Amount),2) AS total_transaction_amount
 FROM transactions;
+
+-- • What are the most common transaction types?
+
+SELECT t2.TypeName AS trans_type, COUNT(t1.TransactionID) AS trans_count
+FROM transactions AS t1
+INNER JOIN transaction_types AS t2
+	ON t1.TransactionTypeID = t2.TransactionTypeID
+GROUP BY t2.TypeName
+ORDER BY trans_count DESC LIMIT 1;
