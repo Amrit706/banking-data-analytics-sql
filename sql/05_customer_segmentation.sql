5f0c75fa2722444e8327558507989d047d375245
Amrit Kumar <amritkumar83728@gmail.com>
2026-09-20 19:49:31 +0530

    High value customers

---
+++ sql/05_customer_segmentation.sql
@@ -0,0 +1,75 @@
+-- ===============================
+-- Customer Segmentation
+-- ===============================
+USE finance_fraud_loans;
+
+-- • High-Value Customers
+-- Customers with high balances and high transaction value.
+
+WITH cte AS
+(
+	SELECT t1.CustomerID, CONCAT_WS(" ",t1.FirstName, t1.LastName) AS customer_name, t2.Balance
+	FROM customers_cleaned AS t1
+	INNER JOIN accounts AS t2
+		ON t1.CustomerID = t2.CustomerID
+	INNER JOIN account_statuses AS t3
+		ON t2.AccountStatusID = t3.AccountStatusID
+	WHERE t3.StatusName = "Active"
+),
+
+cte2 AS 
+(
+	SELECT CustomerID, customer_name, ROUND(SUM(Balance),2) AS total_balance
+	FROM cte
+	GROUP BY CustomerID, customer_name
+	ORDER BY total_balance DESC
+),
+
+cte3 AS
+(
+	SELECT t.CustomerID, t.customer_name, t.total_balance
+	FROM (SELECT CustomerID, customer_name, total_balance,
+			ROUND(((COUNT(*) OVER() ) * 0.2)) AS top_20_percent,
+			ROW_NUMBER() OVER(ORDER BY total_balance DESC) AS rnk
+		  FROM cte2) AS t
+	WHERE t.rnk <= t.top_20_percent
+),
+
+cte4 AS
+(
+	SELECT t1.CustomerID, CONCAT_WS(" ", t1.FirstName, t1.LastName) AS customer_name, t3.TransactionID
+	FROM customers_cleaned AS t1
+	INNER JOIN accounts AS t2
+		ON t1.CustomerID = t2.CustomerID
+	INNER JOIN transactions AS t3
+		ON t2.AccountID = t3.AccountOriginID
+	UNION
+	SELECT t1.CustomerID, CONCAT_WS(" ", t1.FirstName, t1.LastName) AS customer_name, t3.TransactionID
+	FROM customers_cleaned AS t1
+	INNER JOIN accounts AS t2
+		ON t1.CustomerID = t2.CustomerID
+	INNER JOIN transactions AS t3
+		ON t2.AccountID = t3.AccountDestinationID
+),
+
+cte5 AS
+(
+	SELECT CustomerID, customer_name, COUNT(TransactionID) AS trans_counts,
+		ROUND((COUNT(*) OVER() * 0.2)) AS top_20_percent,
+        ROW_NUMBER() OVER(ORDER BY COUNT(TransactionID) DESC) AS rnk
+	FROM cte4
+	GROUP BY CustomerID, customer_name
+),
+
+cte6 AS
+(
+	SELECT CustomerID, customer_name, trans_counts
+	FROM cte5
+	WHERE rnk <= top_20_percent
+)
+
+SELECT t1.CustomerID, t1.customer_name, t1.total_balance, trans_counts
+FROM cte3 AS t1
+INNER JOIN cte6 AS t2
+	ON t1.CustomerID = t2.CustomerID AND t1.customer_name = t2.customer_name
+ORDER BY total_balance DESC, trans_counts DESC;
