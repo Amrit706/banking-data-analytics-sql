292b882658dc981a8a6e2af87d4abc3348d0ef5c
Amrit Kumar <amritkumar83728@gmail.com>
2026-09-24 22:58:07 +0530

    Branch ranking based on multiple performances

--- sql/07_branch_analysis.sql
+++ sql/07_branch_analysis.sql
@@ -54,3 +54,55 @@ SELECT
 FROM transactions
 GROUP BY BranchID
 ORDER BY transaction_value DESC;
+
+-- 3. Rank branches based on multiple performance indicators
+-- • Total account balance
+-- • Transaction volume
+
+WITH branch_balance AS
+(
+	SELECT 
+		CASE WHEN t2.BranchID IS NULL THEN 'Unmarked'
+		ELSE CAST(t2.BranchID AS CHAR)
+		END AS branch,
+		ROUND(SUM(t3.Balance), 2) AS total_account_balance
+	FROM customers_cleaned AS t1
+	LEFT JOIN branches AS t2
+		ON t1.AddressID = t2.AddressID
+	INNER JOIN accounts AS t3
+		ON t1.CustomerID = t3.CustomerID
+	GROUP BY branch
+),
+
+branch_transactions AS
+(
+	SELECT 
+		CAST(BranchID AS CHAR) AS branch,
+		COUNT(TransactionID) AS transaction_volume
+	FROM transactions
+	WHERE BranchID IS NOT NULL
+	GROUP BY BranchID
+),
+
+branch_ranking AS
+(
+	SELECT
+		t1.branch,
+		t1.total_account_balance,
+		COALESCE(t2.transaction_volume, 0) AS transaction_volume,
+		DENSE_RANK() OVER(ORDER BY t1.total_account_balance DESC) AS balance_rank,
+		DENSE_RANK() OVER(ORDER BY COALESCE(t2.transaction_volume, 0) DESC) AS transaction_rank
+	FROM branch_balance AS t1
+	LEFT JOIN branch_transactions AS t2
+		ON t1.branch = t2.branch
+)
+
+SELECT
+	branch,
+	total_account_balance,
+	transaction_volume,
+	balance_rank,
+	transaction_rank,
+	(balance_rank + transaction_rank) AS combined_rank
+FROM branch_ranking
+ORDER BY combined_rank;
