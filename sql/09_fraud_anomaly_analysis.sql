27ccfaf6077c3b4752822da036061be1ed975bc7
Amrit Kumar <amritkumar83728@gmail.com>
2026-09-25 12:49:27 +0530

    Multiple transactions within a short interval

--- sql/09_fraud_anomaly_analysis.sql
+++ sql/09_fraud_anomaly_analysis.sql
@@ -35,3 +35,44 @@ SELECT TransactionID, ROUND(Amount,2) AS transaction_amount,
 FROM cte2
 WHERE rnk <= top_5_percent
 ORDER BY transaction_amount DESC;
+
+-- =================================================
+-- 2. Multiple transactions within a short interval
+-- =================================================
+-- Identify transactions occurring within 5 minutes
+-- of the previous transaction for the same account.
+
+WITH cte AS
+(
+	SELECT t1.AccountOriginID AS AccountID, 
+		t1.TransactionID, 
+		t1.TransactionDate
+	FROM transactions AS t1
+	WHERE t1.AccountOriginID IS NOT NULL
+		AND t1.TransactionDate IS NOT NULL
+	
+	UNION
+	
+	SELECT t1.AccountDestinationID AS AccountID, 
+		t1.TransactionID, 
+		t1.TransactionDate
+	FROM transactions AS t1
+	WHERE t1.AccountDestinationID IS NOT NULL
+		AND t1.TransactionDate IS NOT NULL
+),
+
+cte2 AS
+(
+	SELECT AccountID, TransactionID, TransactionDate,
+		LAG(TransactionDate) OVER(PARTITION BY AccountID ORDER BY TransactionDate) AS previous_transaction_date
+	FROM cte
+)
+
+SELECT AccountID, TransactionID, TransactionDate,
+	previous_transaction_date,
+	TIMESTAMPDIFF(MINUTE, previous_transaction_date, TransactionDate) AS minutes_difference,
+	'Potentially Unusual Short Interval' AS anomaly_indicator
+FROM cte2
+WHERE previous_transaction_date IS NOT NULL
+	AND TIMESTAMPDIFF(MINUTE, previous_transaction_date, TransactionDate) <= 5
+ORDER BY minutes_difference ASC, AccountID, TransactionDate;
