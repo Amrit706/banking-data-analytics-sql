5a8870443d168cf56798a283dffc53cafe0c39ae
Amrit Kumar <amritkumar83728@gmail.com>
2026-09-25 12:08:30 +0530

    Three months running transaction running average

--- sql/08_advanced_analysis.sql
+++ sql/08_advanced_analysis.sql
@@ -55,3 +55,30 @@ SELECT month_year, trans_count AS monthly_transaction,
 	SUM(trans_count) OVER(ORDER BY years ASC, months ASC 
 		ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS Running_total
 FROM cte2;
+
+-- • 3-Month Moving Average
+
+WITH cte AS
+(
+	SELECT *,
+		   CONCAT_WS(" ", MONTHNAME(TransactionDate), YEAR(TransactionDate)) AS month_year,
+		   YEAR(TransactionDate) AS years,
+		   MONTH(TransactionDate) AS months
+	FROM transactions
+),
+
+cte2 AS 
+(
+	SELECT month_year, years, months, COUNT(TransactionID) AS trans_count
+	FROM cte
+	WHERE years IS NOT NULL AND months IS NOT NULL
+	GROUP BY month_year, years, months
+)
+
+SELECT 
+	month_year,
+	trans_count AS monthly_transaction,
+	ROUND(AVG(trans_count) OVER(ORDER BY years ASC, months ASC
+			ROWS BETWEEN 2 PRECEDING AND CURRENT ROW),2) AS three_month_moving_average
+FROM cte2
+ORDER BY years ASC, months ASC;
